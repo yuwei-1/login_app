91a9dc88e3ced1d2dbf85ccdc679bf4a9e0dcf02
yuwei-1 <yuweizhu29@googlemail.com>
2022-06-27 19:02:15 +0100

    moved tkinter mainloop() to main.py and created register page

--- display.py
+++ display.py
@@ -1,35 +1,58 @@
 from tkinter import *
 from tkinter.ttk import *
 
-class loginpage():
 
-    def __init__(self, title, button=None):
+class LoginPage:
+
+    def __init__(self, title, window,  button = None, user = None, pw = None):
         self.title = title
         self.button = button
+        self.window = window
+        self.user = user
+        self.pw = pw
 
     def create_page(self):
 
-        window = Tk()
-        window.title("Login")
-        window.geometry("500x500+500+200")
-        self.window = window
+        self.window.title("Login")
+        self.window.geometry("500x500+500+200")
 
-        self.create_label(window, self.title)
-        self.create_button(window, lambda: self.invalid_login())
-        self.create_field(window, "Username:", 0.25)
-        self.create_field(window, "Password:", 0.35)
+        self.create_label(self.window, self.title)
+        username = self.create_field(self.window, "Username:", 0.25, 15)
+        password = self.create_field(self.window, "Password:", 0.35, 15)
+        self.create_button(self.window, lambda: self.get_login_details(username, password, self.window))
 
-        window.mainloop()
+        style = Style()
+        style.configure('W.TButton', font = ('calibri', 10, 'bold', 'underline'), foreground='red')
 
+        label = Label(self.window, text = "Not a user?", font=("Times 20 italic bold", 10))
+        label.place(relx=0.25, rely=0.45)
+
+        e = Button(self.window, text = "Register", style = "W.TButton", command = lambda: self.register())
+        e.place(relx=0.42, rely=0.45)
+
+    def get_login_details(self, username, password, window):
+
+        self.user = username.get()
+        self.pw = password.get()
 
     def invalid_login(self):
 
         err = Toplevel(self.window)
-        err.geometry("100x100+700+400")
+        err.geometry("200x200+700+400")
         err.title("Login Error")
-        Label(err, text = "Invalid Login").place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.create_label(err, "Invalid Login")
         Button(err, text = "ok", command = err.destroy).place(relx=0.5, rely=0.75, anchor = CENTER)
 
+    def register(self):
+
+        reg = Toplevel(self.window)
+        reg.geometry("500x500+500+200")
+        reg.title("Register Page")
+        self.create_label(reg, "Register")
+        self.create_field(reg, "new username: ", 0.25, 10)
+        self.create_field(reg, "new password: ", 0.35, 10)
+        self.create_field(reg, "confirm password: ", 0.45, 10)
+        Button(reg, text="Back", command=reg.destroy).place(relx=0.5, rely=0.75, anchor=CENTER)
 
     def create_label(self, win, title):
 
@@ -41,16 +64,14 @@ class loginpage():
         b = Button(win, text = self.button, width = 20, command = command)
         b.place(relx=0.5, rely=0.75, anchor=CENTER)
 
-    def create_field(self, win, name, ypos):
+    def create_field(self, win, name, ypos, textsize):
 
-        label = Label(win, text = name, font=("Times 20 italic bold", 15))
+        label = Label(win, text = name, font=("Times 20 italic bold", textsize))
         label.place(relx=0.25, rely=ypos)
 
         e = Entry(win)
         e.place(relx=0.5, rely=ypos)
 
+        return e
 
 
-p1 = loginpage("Login page", "Login")
-
-p1.create_page()
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from display import LoginPage
+import sqlite3
+from tkinter import *
+from tkinter.ttk import *
+
 if __name__ == "__main__":
 
-    print("hello world")
+    window = Tk()
+
+    p1 = LoginPage("Login page", window, "Login")
+    p1.create_page()
+
+    while True:
+
+        if p1.user:
+            print(p1.user)
+            p1.user = None
+
+        window.update_idletasks()
+        window.update()
