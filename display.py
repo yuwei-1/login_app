42fe1ce7bc36a8c3cecff68b7bf969a0510ea70e
yuwei-1 <yuweizhu29@googlemail.com>
2022-07-27 16:06:09 +0100

    added a landing page

--- display.py
+++ display.py
@@ -1,23 +1,26 @@
+import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
+from PIL import ImageTk, Image
 
 
 class LoginPage:
 
-    def __init__(self, title, window,  button = None, user = None, pw = None, email = None, newuser = None, newpw = None, reg = None):
+    def __init__(self, title, window,  button=None):
         self.title = title
         self.button = button
         self.window = window
-        self.user = user
-        self.pw = pw
-        self.email = email
-        self.newuser = newuser
-        self.newpw = newpw
-        self.reg = reg
+        self.user = None
+        self.pw = None
+        self.email = None
+        self.new_user = None
+        self.new_pw = None
+        self.confirm_pw = None
+        self.reg = None
         self.code = None
         self.auth = None
 
-    def create_page(self):
+    def create_login_page(self):
 
         self.window.title("Login")
         self.window.geometry("500x500+500+200")
@@ -32,12 +35,12 @@ class LoginPage:
     def create_register(self, window):
 
         style = Style()
-        style.configure('W.TButton', font = ('calibri', 10, 'bold', 'underline'), foreground='red')
+        style.configure('W.TButton', font=('calibri', 10, 'bold', 'underline'), foreground='red')
 
-        label = Label(window, text = "Not a user?", font=("Times 20 italic bold", 10))
+        label = Label(window, text="Not a user?", font=("Times 20 italic bold", 10))
         label.place(relx=0.3, rely=0.45)
 
-        e = Button(window, text = "Register", style = "W.TButton", command = lambda: self.register())
+        e = Button(window, text="Register", style="W.TButton", command=lambda: self.register())
         e.place(relx=0.5, rely=0.44)
 
     def create_email_authenticator(self):
@@ -49,7 +52,8 @@ class LoginPage:
         authenticator.title("Email Authenticator")
         self.create_label(authenticator, "A 6-Digit code has been sent\nto the email you used for registration.")
         code = self.create_field(authenticator, "Code:", 0.4, 15)
-        Button(authenticator, text = "Verify", command = lambda: self.get_code(code)).place(relx=0.5, rely=0.75, anchor = CENTER)
+        Button(authenticator, text="Verify",
+               command=lambda: self.get_code(code)).place(relx=0.5, rely=0.75, anchor=CENTER)
 
     def get_login_details(self, username, password):
 
@@ -60,11 +64,11 @@ class LoginPage:
 
         self.code = code.get()
 
-    def get_new_login(self, newuser, newpw, confirmpw, email):
+    def get_new_login(self, new_user, new_pw, confirm_pw, email):
 
-        self.newuser = newuser.get()
-        self.newpw = newpw.get()
-        self.confirmpw = confirmpw.get()
+        self.new_user = new_user.get()
+        self.new_pw = new_pw.get()
+        self.confirm_pw = confirm_pw.get()
         self.email = email.get()
 
     def invalid_login(self):
@@ -73,7 +77,7 @@ class LoginPage:
         err.geometry("200x200+700+400")
         err.title("Login Error")
         self.create_label(err, "Invalid Login")
-        Button(err, text = "ok", command = err.destroy).place(relx=0.5, rely=0.75, anchor = CENTER)
+        Button(err, text="ok", command=err.destroy).place(relx=0.5, rely=0.75, anchor=CENTER)
 
     def successful_login(self):
 
@@ -81,7 +85,7 @@ class LoginPage:
         success.geometry("200x200+700+400")
         success.title("Login Successful")
         self.create_label(success, "Login Successful")
-        Button(success, text = "ok", command = success.destroy).place(relx=0.5, rely=0.75, anchor = CENTER)
+        Button(success, text="ok", command=success.destroy).place(relx=0.5, rely=0.75, anchor=CENTER)
 
     def register(self):
 
@@ -95,36 +99,58 @@ class LoginPage:
         new_user = self.create_field(reg, "new username: ", 0.27, 10)
         new_pass = self.create_field(reg, "new password: ", 0.34, 10)
         confirm_new_pass = self.create_field(reg, "confirm password: ", 0.41, 10)
-        emailaddress = self.create_field(reg, "email address: ", 0.48, 10)
+        email_address = self.create_field(reg, "email address: ", 0.48, 10)
 
         Button(reg, text="Back", command=lambda: reg.destroy()).place(relx=0.5, rely=0.75, anchor=CENTER)
-        Button(reg, text="Register", command=lambda: self.get_new_login(new_user, new_pass, confirm_new_pass, emailaddress)).place(relx=0.5, rely=0.65, anchor=CENTER)
+        Button(reg, text="Register", command=lambda: self.get_new_login(
+            new_user, new_pass, confirm_new_pass, email_address)).place(relx=0.5, rely=0.65, anchor=CENTER)
 
-    def create_label(self, win, title):
-
+    @staticmethod
+    def create_label(win, title):
         l1 = Label(win, text=title, font=("Times 20 italic bold", 14))
         l1.pack()
 
     def create_button(self, win, command):
 
-        b = Button(win, text = self.button, width = 20, command = command)
+        b = Button(win, text=self.button, width=20, command=command)
         b.place(relx=0.5, rely=0.75, anchor=CENTER)
 
-    def create_field(self, win, name, ypos, textsize):
+    @staticmethod
+    def create_field(win, name, y_pos, text_size):
 
-        label = Label(win, text = name, font=("Times 20 italic bold", textsize))
-        label.place(relx=0.25, rely=ypos)
+        label = Label(win, text=name, font=("Times 20 italic bold", text_size))
+        label.place(relx=0.25, rely=y_pos)
 
         if "password" in name.lower():
-            e = Entry(win, show = "●")
+            e = Entry(win, show="●")
         else:
             e = Entry(win)
-
-        e.place(relx=0.5, rely=ypos)
-
+        e.place(relx=0.5, rely=y_pos)
         return e
 
-    def display_message(self, win, msg):
+    @staticmethod
+    def display_message(win, msg):
 
-        l2 = Label(win, text = msg)
-        l2.place(relx = 0.35, rely=0.55)
+        l2 = Label(win, text=msg)
+        l2.place(relx=0.35, rely=0.55)
+
+
+class LandingPage(LoginPage):
+
+    def __init__(self, window, user, email):
+
+        self.window = window
+        self.user = user
+        self.email = email
+        self.img = ImageTk.PhotoImage(Image.open("Images/fsocietywallpaper.png"))
+
+    def create_landing_page(self):
+
+        self.window.configure(bg='black')
+        self.window.title("Landing Page")
+        self.window.geometry("1350x700+0+50")
+        super().create_label(self.window, "Landing Page\n")
+        super().create_label(self.window, "Welcome, " + self.user)
+
+        label = tk.Label(image=self.img, borderwidth=0, highlightthickness=0)
+        label.pack(padx=0, pady=0)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from display import LoginPage
+from display import LoginPage, LandingPage
 from tkinter import *
 from queries import DatabaseQueries
 from emailauth import EmailHandler
@@ -6,12 +6,25 @@ from emailauth import EmailHandler
 
 if __name__ == "__main__":
 
+    login_success = False
+
     window = Tk()
 
     p1 = LoginPage("Login page", window, "Login")
-    p1.create_page()
+    p1.create_login_page()
 
     while True:
+
+        if login_success:
+
+            window.destroy()
+            window = Tk()
+
+            user, email = login_success
+            p2 = LandingPage(window, user, email)
+            p2.create_landing_page()
+
+            login_success = False
 
         if p1.user:
             q1 = DatabaseQueries()
@@ -30,8 +43,8 @@ if __name__ == "__main__":
                     if p1.code:
 
                         if p1.code == correct_code:
-
                             p1.successful_login()
+                            login_success = (p1.user, receiver_email)
                         else:
                             p1.display_message(window, "Your code is incorrect.")
 
@@ -40,9 +53,10 @@ if __name__ == "__main__":
                         break
 
                     try:
+                        p1.auth.protocol("WM_DELETE_WINDOW", lambda: p1.auth.destroy())
                         p1.auth.update_idletasks()
                         p1.auth.update()
-                    except:
+                    except TclError:
                         break
 
             else:
@@ -51,19 +65,19 @@ if __name__ == "__main__":
             p1.user = None
             p1.pw = None
 
-        if p1.newuser:
+        if p1.new_user:
 
-            e1 = EmailHandler(receiver_email = p1.email)
+            e1 = EmailHandler(receiver_email=p1.email)
 
-            if p1.confirmpw == p1.newpw:
+            if p1.confirm_pw == p1.new_pw:
 
                 q1 = DatabaseQueries()
-                exists = q1.check_new(p1.newuser, p1.email)
+                exists = q1.check_new(p1.new_user, p1.email)
 
                 if not exists:
 
                     if e1.send_email():
-                        q1.create_new(p1.newuser, p1.newpw, p1.email)
+                        q1.create_new(p1.new_user, p1.new_pw, p1.email)
                         p1.reg.destroy()
                         p1.display_message(window, "Registration was Successful")
                     else:
@@ -78,14 +92,10 @@ if __name__ == "__main__":
 
                 p1.display_message(p1.reg, "The passwords do not match")
 
-
-            p1.newuser = None
+            p1.new_user = None
 
         try:
             window.update_idletasks()
             window.update()
-
-        except:
-
+        except TclError:
             break
-
